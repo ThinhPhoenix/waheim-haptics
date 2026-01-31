0c07162adfffea715de99f786c661d60b6c75a37
ThinhPhoenix <laithinh1310@gmail.com>
2026-01-31 12:12:31 +0700

    update

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-    interface Navigator {
-        vibrate(pattern: number | number[]): boolean;
-    }
-}
-export declare const triggerHaptics: (pattern?: number | number[]) => boolean;
-export declare const useHaptics: () => (pattern?: number | number[]) => void;
